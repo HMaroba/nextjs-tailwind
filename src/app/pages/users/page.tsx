0f1c21a629165bb4932e97156a0651856b80e174
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-04 20:37:18 +0200

    Fixed pages

--- src/app/pages/users/page.tsx
+++ src/app/pages/users/page.tsx
@@ -1,36 +1,42 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+
   const getUsers = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+        console.log(users);
+      });
   };
 
   useEffect(() => {
-   try {
+    try {
       getUsers();
-   } catch (error) {
-    
-   }
+    } catch (error) {}
   });
 
   return (
     <>
       <h1 className="text-4xl text-center mt-5 font-semibold">Users</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 mt-10 w-full lg:space-x-8 p-3 ">
-        <div className="h-39 md:h-80 mt-4 lg:mt-1 shadow-lg rounded-md p-2 hover:shadow-md">
-          <h6 className="mt-2 mb-5 font-semibold text-3xl">Startup Trail</h6>
-          <ul>
-            <p>
-              On the scheduled date and time, our team arrives at the customers
-              location for pickup (if selected).
-            </p>
-            <p>The customer hands over the laundry.</p>
-          </ul>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 mt-10 w-full lg:space-x-8 p-3 ">
+        {users.map((user: any) => (
+          <>
+            <div className="h-39 md:h-60 mt-4 lg:mt-1 shadow-lg rounded-md p-2 hover:shadow-md">
+              <h6 className="mt-2 mb-5 font-semibold text-3xl">{user.name}</h6>
+              <ul>
+                <p>{user.email}</p>
+                <p>{user.address.street}</p>
+                <p>{user.address.city}</p>
+              </ul>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
