b4a20f4e2a6f8259cba7bff12084fc3ec087045a
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-04 20:18:42 +0200

    Fixed pages

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <>
       <div className="mt-5">
         <h1 className="text-4xl text-center mt-5 font-semibold">About us</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full lg:space-x-8 p-3 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full p-3 ">
           <div className="h-39 md:h-80  shadow-lg rounded-md p-2 hover:shadow-md">
             <h6 className="mt-2 mb-5 font-semibold text-3xl">About Us</h6>
           </div>
--- src/app/pages/pricing/page.tsx
+++ src/app/pages/pricing/page.tsx
@@ -3,10 +3,10 @@ import About from "../about/page";
 export default function Pricing() {
   return (
     <>
-    <About />
+    
       <h1 className="text-4xl text-center mt-5 font-semibold">Our Pricing</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 mt-10 w-full lg:space-x-8 p-3 ">
-        <div className="h-39 md:h-80 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
+        <div className="h-39 md:h-80 mt-4 lg:mt-1 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">Startup Trail</h6>
           <ul>
             <p>
@@ -24,7 +24,7 @@ export default function Pricing() {
             Submit
           </button>
         </div>
-        <div className="h-40 md:h-80 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
+        <div className="h-40 md:h-80 mt-4 lg:mt-1 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">Free Trail</h6>
           <ul>
             <p>
@@ -34,7 +34,7 @@ export default function Pricing() {
             <p>The customer hands over the laundry.</p>
           </ul>
         </div>
-        <div className="h-39 md:h-80 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
+        <div className="h-39 md:h-80 mt-4 lg:mt-1 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">PRO PLAN</h6>
           <ul>
             <p>
