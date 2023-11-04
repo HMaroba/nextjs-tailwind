6f16d9f5e187b68ca55438a2c2d8612e67c062d0
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-04 20:25:39 +0200

    Fixed pages

--- src/app/pages/users/page.tsx
+++ src/app/pages/users/page.tsx
@@ -1,32 +1,28 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Users() {
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  useEffect(() => {
+   try {
+      getUsers();
+   } catch (error) {
+    
+   }
+  });
+
   return (
     <>
       <h1 className="text-4xl text-center mt-5 font-semibold">Users</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 mt-10 w-full lg:space-x-8 p-3 ">
         <div className="h-39 md:h-80 mt-4 lg:mt-1 shadow-lg rounded-md p-2 hover:shadow-md">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">Startup Trail</h6>
-          <ul>
-            <p>
-              On the scheduled date and time, our team arrives at the customers
-              location for pickup (if selected).
-            </p>
-            <p>The customer hands over the laundry.</p>
-          </ul>
-        </div>
-        <div className="h-40 md:h-80 mt-4 lg:mt-1 shadow-lg rounded-md p-2 hover:shadow-md">
-          <h6 className="mt-2 mb-5 font-semibold text-3xl">Free Trail</h6>
-          <ul>
-            <p>
-              On the scheduled date and time, our team arrives at the customers
-              location for pickup (if selected).
-            </p>
-            <p>The customer hands over the laundry.</p>
-          </ul>
-        </div>
-        <div className="h-39 md:h-80 mt-4 lg:mt-1 shadow-lg rounded-md p-2 hover:shadow-md">
-          <h6 className="mt-2 mb-5 font-semibold text-3xl">PRO PLAN</h6>
           <ul>
             <p>
               On the scheduled date and time, our team arrives at the customers
