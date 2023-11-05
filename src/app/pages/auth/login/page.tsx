fcb8c75e7021ca582e5a981e376755f8dc252e60
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 10:28:12 +0200

    Modified register page

--- src/app/pages/auth/login/page.tsx
+++ src/app/pages/auth/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
-            <div>
+            {/* <div>
               <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -30,9 +30,42 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
+            </div> */}
+            <div className="w-full">
+              <label
+                className="block my-2 text-left  
+                                          text-sm font-medium text-gray-900"
+              >
+               Email Address
+              </label>
+              <input
+                type="text"
+                className="shadow-sm bg-gray-50 border 
+                                          border-gray-300 text-gray-900  
+                                          text-sm rounded-md block w-full p-2.5"
+                placeholder="Enter email address"
+                required
+              />
             </div>
 
             <div>
+            <label
+              className="block my-2 text-left  
+                                          text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="shadow-sm bg-gray-50 border 
+                                          border-gray-300 text-gray-900  
+                                          text-sm rounded-md block w-full p-2.5"
+            
+              required
+            />
+          </div>
+
+            {/* <div>
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
@@ -59,7 +92,7 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
             <div>
               <button
