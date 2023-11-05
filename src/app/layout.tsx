b7dbd6095e203e7ae90a43ff651367f8e52691c0
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 06:22:12 +0200

    Modified register page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import SideNavbar from './pages/navigation/Sidebar'
 
 const inter = Open_Sans({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      <SideNavbar />
+        {children}
+        </body>
     </html>
   )
 }
