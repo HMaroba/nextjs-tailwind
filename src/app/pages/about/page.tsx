fc7f36ee023879b4a6739c9c298f0c9b59e2c612
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 06:20:11 +0200

    Modified register page

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -3,11 +3,11 @@ export default function About() {
     <>
       <div className="mt-5">
         <h1 className="text-4xl text-center mt-5 font-semibold">About us</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full p-3 ">
-          <div className="h-39 md:h-80  shadow-lg rounded-md p-2 hover:shadow-md">
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full p-8 ">
+          <div className="h-39 md:h-80 ">
             <h6 className="mt-2 mb-5 font-semibold text-3xl">About Us</h6>
           </div>
-          <div className="h-40 md:h-80  shadow-lg rounded-md p-2 hover:shadow-md">
+          <div className="h-40 md:h-80">
             <h6 className="mt-2 mb-5 font-semibold text-3xl">Free Trail</h6>
             <ul>
               <p>
