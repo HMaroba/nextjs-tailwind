7b5bc6ba5e7458e7f91ce8b4c0b69d4dc2ce1774
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 10:14:22 +0200

    Modified register page

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <div className="mt-5">
         <h1 className="text-4xl text-center mt-5 font-semibold">About us</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full p-8 ">
-          <div className="h-39 md:h-80 ">
+          <div className="h-39 md:h-80 hidden lg:block ">
             <h6 className="mt-2 mb-5 font-semibold text-3xl">About Us</h6>
           </div>
           <div className="h-40 md:h-80">
