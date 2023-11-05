8a71b173df1fb5233a8aef225329b30d370b57d9
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 06:11:49 +0200

    Modified register page

--- src/app/pages/users/page.tsx
+++ src/app/pages/users/page.tsx
@@ -24,10 +24,10 @@ export default function Users() {
   return (
     <>
       <h1 className="text-4xl text-center mt-5 font-semibold">Users</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 p-5 lg:grid-cols-3 mt-10 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 mt-10 w-full gap-6 p-2 lg:p-6">
         {users.map((user: any) => (
           <>
-            <div className="h-39 md:h-60 mt-4 p-5 lg:mt-1 pr-8 shadow-lg rounded-md p-2 hover:shadow-md">
+            <div className="h-39 md:h-60 mt-4 lg:mt-1 shadow-lg rounded-md hover:shadow-md p-3 w-full">
               <h6 className="mt-2 mb-5 font-semibold text-3xl">{user.name}</h6>
               <ul>
                 <p>{user.email}</p>
