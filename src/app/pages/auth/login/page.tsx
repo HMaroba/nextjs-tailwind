19ad35fad460741198e46ec6b948728595505307
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 07:30:50 +0200

    Modified register page

--- src/app/pages/auth/login/page.tsx
+++ src/app/pages/auth/login/page.tsx
@@ -25,6 +25,7 @@ export default function Login() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  placeholder="Enter email address"
                   required
                   className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
