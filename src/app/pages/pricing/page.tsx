5f7aa437e81e4eae58b1d43b10958ab299a2aeed
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-05 06:16:14 +0200

    Modified register page

--- src/app/pages/pricing/page.tsx
+++ src/app/pages/pricing/page.tsx
@@ -4,8 +4,8 @@ export default function Pricing() {
   return (
     <>
       <h1 className="text-4xl text-center mt-5 font-semibold">Our Pricing</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 mt-10 w-full p-5 ">
-        <div className="mt-4 lg:mt-1 shadow-2xl rounded-md  p-3 hover:shadow-md lg:mr-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-10 w-full gap-5 p-4 ">
+        <div className="mt-4 lg:mt-1 shadow-2xl rounded-md  p-3 hover:shadow-md ">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">Startup Trail</h6>
           <ul>
             <p>
@@ -23,7 +23,7 @@ export default function Pricing() {
             </button>
        
         </div>
-        <div className=" mt-4 lg:mt-1 shadow-lg rounded-md p-3 hover:shadow-md lg:mr-4">
+        <div className=" mt-4 lg:mt-1 shadow-lg rounded-md p-3 hover:shadow-md">
           <h6 className="mt-2 mb-5 font-semibold text-3xl">Free Trail</h6>
           <ul>
             <p>
