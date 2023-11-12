d483684e543d740ee7af44d6ee27bf01038ba0e3
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-12 22:10:12 +0200

    Fixed logo

--- src/app/(components)/footer.tsx
+++ src/app/(components)/footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaInbox, FaLocationArrow, FaPhoneAlt } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -8,7 +9,7 @@ export default function Footer() {
           <div className="h-40">
             <h6 className="mt-2 mb-5 font-semibold text-xl">About</h6>
             <ul>
-            <div>
+              <div>
                 <Link href={"#"}>About 1</Link>
               </div>
               <div>
@@ -22,7 +23,7 @@ export default function Footer() {
           <div className="h-40">
             <h6 className="mt-2 mb-5 font-semibold text-xl">Features</h6>
             <ul>
-            <div>
+              <div>
                 <Link href={"#"}>About 1</Link>
               </div>
               <div>
@@ -36,7 +37,7 @@ export default function Footer() {
           <div className="h-40">
             <h6 className="mt-2 mb-5 font-semibold text-xl">Pricing</h6>
             <ul>
-            <div>
+              <div>
                 <Link href={"#"}>About 1</Link>
               </div>
               <div>
@@ -49,17 +50,31 @@ export default function Footer() {
           </div>
           <div className="h-40">
             <h6 className="mt-2 mb-5 font-semibold text-xl">Contact</h6>
-            <ul>
-            <div>
-                <Link href={"#"}>Email : hlalelegmail.com</Link>
+            <ul className="space-y-2">
+              <div className="flex flex-row space-x-3">
+                <FaLocationArrow />
+                <Link href={"#"}>hlalelegmail.com</Link>
               </div>
-              <div>
-                <Link href={"#"}>Location : Maseru Lestho  1</Link>
+              <div className="flex flex-row space-x-3">
+              <FaLocationArrow />
+                <Link href={"#"}>Location : Maseru Lestho 1</Link>
               </div>
-              <div>
+              <div className="flex flex-row space-x-3">
+                <FaPhoneAlt />
                 <Link href={"#"}>Phone : +266 78901234</Link>
               </div>
             </ul>
+            {/* <div className="flex flex-row mt-4 space-x-10">
+              <div className="text-2xl text-blue-500">
+                <FaLocationArrow  />
+              </div>
+              <div className="text-2xl text-blue-500">
+                <FaPhoneAlt />
+              </div>
+              <div className="text-2xl text-blue-500">
+                <FaInbox />
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
