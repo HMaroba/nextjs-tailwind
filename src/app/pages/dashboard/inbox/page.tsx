b004b248ec90bcd169c65da0c265b5dedcd63678
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-13 14:25:59 +0200

    Added Dashboard

---
+++ src/app/pages/dashboard/inbox/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Inbox(){
+    return(
+        <div className="p-10 text-blue-500">
+            <h6 className="text-3xl text-center">No Messages found</h6>
+        </div>
+    )
+}
