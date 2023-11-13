4014994d9549497dcfeb35e3789f7f8d58db89fc
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-13 14:32:24 +0200

    Added Dashboard

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+
+export default function NotFound() {
+  return (
+    <>
+      <div className="min-h-100vh">
+        <div>
+        <p className="text-6xl">Page not Found</p>
+        <p>Could not find requested resource</p>
+        </div>
+      </div>
+    </>
+  );
+}
